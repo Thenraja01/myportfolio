2c8fcefbc79ecd53ba6fcf1bd6097edc5fbc018a
Thenraja01 <thenwthen@gmail.com>
2025-06-03 18:32:07 +0530

    done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import person from '../components/download.jpeg'
 import styles from '../../public/style/Home.module.css'
 export default function Home() {
+  data={
+  username:"Then Raja",
+  joblevel:"Web devoloper",
+  jobdesc:"A Web developer who focus on the user interface and user experience side of website and web application .i specialised with JS library likes React JS and freaquently update my skills. ",
+  }
   return (
     <div className={styles.content} id='home'>
         <div className={styles.contentmain}>
         <img src={person} alt="personimage" />
-        <h1>Then Raja</h1>
-        <h3>Web devoloper</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, tempore a amet animi fugiat nisi sed explicabo saepe impedit, velit suscipit nam rem excepturi dolore inventore incidunt quisquam natus in?</p>
+        <h1>{data.username}</h1>
+        <h3>{data.joblevel}</h3>
+            <p>{data.jobdesc}</p>
         </div>
         <div className={styles.aboutcontent} id='about'>
         <h3>About me</h3>
         <img src={person} alt="" />
         <p>
         <h1>HI EVERY ONE</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam asperiores ducimus possimus aut consequuntur doloremque omnis! Reprehenderit porro ex quidem molestias in dignissimos reiciendis voluptatibus, earum nobis doloribus asperiores!</p>
-
+        {data.jobdesc}
+</p>
         </div>
     </div>
   )
